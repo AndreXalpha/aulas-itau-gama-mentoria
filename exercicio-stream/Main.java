401d8b204c62e10912be5a65aa10ec405a3a5c5b
André Luis dos Santos Silva Junior <andreluis_alpha1@hotmail.com>
2023-07-19 10:33:00 -0300

    Primeiro commit, stream, microserviços Hospital

---
+++ exercicio-stream/Main.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+// Listar todas as pessoas
+// Listar as 10 primeiras pessoas
+// Listar as pessoas que são do Brasil
+// Listar as pessoas do Brasil com o nome em Uppercase e suas idades
+// Listar as pessoas que são do Brasil, em ordem de nome
+// Listar as pessoas que são do Brasil, em ordem de idade
+// Contar quantas pessoas tem mais de 45 anos
+// Calcular a média de idade das pessoas
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = Dados.getPessoas();
+
+        // Listar todas as pessoas
+        pessoas.stream().forEach(System.out::println);
+
+        // Listar as 10 primeiras pessoas
+        pessoas.stream()
+                .limit(10)
+                .forEach(System.out::println);
+
+        // Listar as pessoas que são do Brasil
+        System.out.println("\nPessoas do Brasil");
+        pessoas.stream()
+                .filter(p -> p.getPais()
+                .equals("Brazil"))
+                .forEach(System.out::println);
+
+        // Listar as pessoas do Brasil com o nome em Uppercase e suas idades
+        System.out.println("\nPessoas do Brasil com nome maiusculo ");
+        pessoas.stream()
+                .filter(p -> p.getPais()
+                .equals("Brazil"))
+                .map(p -> p.getNome().toUpperCase() + ", " + p.getIdade() + ", " + p.getPais())
+                .forEach(System.out::println);
+
+    }
+
+}
