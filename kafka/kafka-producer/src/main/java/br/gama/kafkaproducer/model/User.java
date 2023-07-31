e3c57f791bd1b7a218cddb1a96a5676cb87a9e9e
André Luis dos Santos Silva Junior <andreluis_alpha1@hotmail.com>
2023-07-30 22:45:20 -0300

    trabalho da aula do dia 12/07/2023

---
+++ kafka/kafka-producer/src/main/java/br/gama/kafkaproducer/model/User.java
@@ -0,0 +1,12 @@
+package br.gama.kafkaproducer.model;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+
+@Getter
+public class User implements Serializable{
+    private int id;
+    private String name;
+    
+}
